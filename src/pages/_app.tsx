e88e6b6c4f1bd951ea207bba6ef8947437ed4118
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-10-31 02:32:11 -0300

    feat: add chakra-ui dependencies and its config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
-import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+
+import { theme } from '../styles/theme';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
