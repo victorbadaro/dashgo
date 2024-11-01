e1ed79db495fcbeb6498a41c8aeb48e60b6c027b
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-01 18:07:40 -0300

    feat: make header responsive

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./logo";
 import { NotificationsNav } from "./notifications-nav";
 import { Profile } from "./profile";
 import { SearchBox } from "./search-box";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  });
+
   return (
     <Flex
       as="header"
@@ -18,11 +23,13 @@ export function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && (
+        <SearchBox />
+      )}
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
