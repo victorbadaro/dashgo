a60e8b6b67064490b1af7abf2381561bda840bb9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-02 00:06:48 -0300

    feat: add navigation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 import { Input } from "../../components/form/input";
 import { Header } from "../../components/header";
 import { Sidebar } from "../../components/sidebar";
@@ -30,7 +31,9 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
