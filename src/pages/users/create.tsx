52ea0ee6ee86063ee4a390308810377e56e28333
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-05 17:21:39 -0300

    feat: add react query mutation to create users

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,12 +1,15 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import { yupResolver } from '@hookform/resolvers/yup';
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 import * as yup from 'yup';
 import { Input } from "../../components/form/input";
 import { Header } from "../../components/header";
 import { Sidebar } from "../../components/sidebar";
-
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   name: string;
@@ -23,14 +26,28 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('/users', {
+      user: {
+        ...user,
+        created_at: new Date()
+      }
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    }
+  });
   const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
     resolver: yupResolver(createUserFormSchema)
   });
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000)); // 2 seconds (fake)
-
-    console.log(values);
+    await createUser.mutateAsync(values);
+    router.push('/users');
   }
 
   return (
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { ActiveModelSerializer, createServer, Factory, Model, Response } from 'miragejs';
 
 type User = {
   name: string;
@@ -9,6 +9,10 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer
+    },
+
     models: {
       user: Model.extend<Partial<User>>({} as User)
     },
@@ -28,7 +32,7 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 200);
+      server.createList('user', 20);
     },
 
     routes() {
